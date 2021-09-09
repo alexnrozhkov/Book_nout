ba712cb62f872ea2fee9efda6bb50bb5f917f90e
Александр <89692443+alexnrozhkov@users.noreply.github.com>
2021-09-10 00:02:13 +0300

    Add files via upload

--- app/src/main/java/com/example/book/MainActivity.java
+++ app/src/main/java/com/example/book/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.book;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+
+    public void addListenerOnButton() {
+        TextView lesson1 = (TextView) findViewById(R.id.textView2);
+        lesson1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(".lesson1");
+                        startActivity(intent);
+                    }
+                }
+        );
+
     }
 }
